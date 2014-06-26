b3ab9123e0a13bc011f6b1222af4aab5fdb9f52e
Christina Koch <christinakconnect@gmail.com>
2014-06-26 07:40:45 -0700

    Unit testing exercise at Reading Workshop

--- Python/sys-version.py
+++ Python/sys-version.py
@@ -1,3 +1,6 @@
 import sys
 
 print 'version is', sys.version
+print 'arguments are', sys.argv
+
+print 'hello',sys.argv[1]
---
+++ Shell/filesystem/users/josie/species-mars-rover/2012-07-03/loop_goostat.sh
@@ -0,0 +1,2 @@
+for filename in *[AB].txt; do   echo $filename;  bash goostats $filename loop-$filename; done
+
